3c27e85f74299b5551072bb62c51beafc4b01307
PROD\mahmad <marwa.ahmad8@gmail.com>
2023-10-23 13:12:20 +0200

    Added Service layer

---
+++ src/Robot.Service/IRobotService.cs
@@ -0,0 +1,16 @@
+﻿using Robot.Model;
+
+namespace Robot.Service
+{
+    public interface IRobotService
+    {
+        /// <summary>
+        /// Implement the robot cleaning logic and database interaction here
+        /// Update the database with the result
+        /// Return the result in JSON format
+        /// </summary>
+        /// <param name="commandRequest"></param>
+        /// <returns></returns>
+        public object ExecutePath(RobotCommandRequest commandRequest);
+    }
+}
